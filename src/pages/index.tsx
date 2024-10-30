b385ed718f1ea561e42233e72e51ae530d25b062
gottajsx <gottajsx@gmail.com>
2024-10-30 21:35:54 +0100

    Adding temporary forms to add problems into db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,47 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
 import useHasMounted from "@/hooks/useHasMounted";
-
+/* START temp form */
 import { useState } from "react";
+import { firestore } from "@/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export default function Home() {
 	const [loadingProblems, setLoadingProblems] = useState(true);
 	const hasMounted = useHasMounted();
+
+	/* START temp form */
+	const [inputs, setInputs] = useState({
+		id: '',
+		title: '',
+		difficulty: '',
+		category: '',
+		videoid: '',
+		link: '',
+		order: 0,
+		likes: 0, 
+		dislikes: 0
+	})
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputs({
+			...inputs,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const newProblem={
+			...inputs,
+			order: Number(inputs.order)
+		}
+		// convert inputs.order to integer 
+
+		await setDoc(doc(firestore, "problems", inputs.id), newProblem)
+		alert("Save to db")
+	}
+	/* END temp form */
 
 	if (!hasMounted) return null;
 
@@ -54,6 +89,20 @@ export default function Home() {
 						<ProblemsTable setLoadingProblems={setLoadingProblems} />
 					</table>
 				</div>
+
+				{/* START temp form */}
+				<form className="p-6 flex flex-col max-w-sm gap-3" onSubmit={handleSubmit}>
+					<input onChange={handleInputChange} type="text" placeholder="problem id" name="id" />
+					<input onChange={handleInputChange} type="text" placeholder="title" name="title" />
+					<input onChange={handleInputChange} type="text" placeholder="difficulty" name="difficulty" />
+					<input onChange={handleInputChange} type="text" placeholder="category" name="category" />
+					<input onChange={handleInputChange} type="text" placeholder="order" name="order" />
+					<input onChange={handleInputChange} type="text" placeholder="videoid" name="videoid" />
+					<input onChange={handleInputChange} type="text" placeholder="link" name="link" />
+					<button className="bg-white">Save to db</button>
+				</form>
+				{/* END temp form */}
+
 			</main>
 		</>
 	);
